3ea326587637a66657180363033704366c302223
Saoud Salah Eddine <salah_fh@outlook.com>
2026-01-18 12:22:30 -0600

    Tweaking keympas.

--- nvim/.config/nvim/lua/config/keymap.lua
+++ nvim/.config/nvim/lua/config/keymap.lua
@@ -4,4 +4,6 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Vim keymaps' })
+vim.keymap.set('n', '<leader>fc', builtin.git_commits, { desc = 'Search git commits' })
 vim.keymap.set('n', '<leader>gd', gitsigns.toggle_deleted, { desc = 'Toggle git sign' })
