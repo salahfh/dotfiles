95b845db221f593c0b3eccb1b308c335b29436b0
Saoud Salah Eddine <salah_fh@outlook.com>
2026-01-18 22:18:47 -0600

    Fix the vim global unknown in lua lsp.

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,27 +1,34 @@
 -- type :help lspconfig-all (to find all related information to the lsp)
 -- for lsp commands: search :help ins-completion
-return { 
+return {
     "neovim/nvim-lspconfig",
-    dependancies = { 
+    dependancies = {
       "folke/lazydev.nvim",
       ft = "lua", -- only load on lua files
       opts = {
         library = {
-          -- See the configuration section for more details
-          -- Load luvit types when the `vim.uv` word is found
           { path = "${3rd}/luv/library", words = { "vim%.uv" } },
         },
       },
     },
-    config = function() 
+    config = function()
       require("lspconfig").lua_ls.setup {
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" }
-            }
-          }
-        }
-      }
-    end,
-  }
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim' },
+          },
+          workspace = {
+            library = {
+              vim.env.VIMRUNTIME,  
+              vim.fn.stdpath('config'),  
+            },
+          },
+          runtime = {
+            version = 'LuaJIT',  
+          },
+        },
+      },
+    }
+  end
+}
